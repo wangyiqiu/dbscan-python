7a5ee1a0c9e420cd6567fb0d54c5df83355870c1
anivegesana <anirudh.vegesana@gmail.com>
2022-11-29 11:47:28 -0800

    Automatic versioning from Git tags

--- setup.py
+++ setup.py
@@ -1,7 +1,11 @@
 import setuptools
 from setuptools.extension import Extension
+import setuptools_scm
 import numpy
+
 import ast
+import glob
+import json
 import sys
 import os
 
@@ -20,18 +24,6 @@ except:
     # Give up if it doesn't work. Not a big deal.
     pass
 
-# Read README.md and set it as the description
-with open("README.md", "r", encoding="utf-8") as fh:
-    long_description = fh.read()
-
-# Get the module version from src/dbscanmodule.cpp
-with open("src/dbscanmodule.cpp", "r") as fh:
-    version = fh.readline().strip()
-    assert version.startswith('#define DBSCAN_VERSION ')
-    version = ast.literal_eval(version.replace('#define DBSCAN_VERSION ', ''))
-    assert isinstance(version, str)
-
-
 if os.name == 'nt':
     # Windows compile time arguments
     extra_compile_args = ["/std:c++17", "/Ot"]
@@ -39,42 +31,31 @@ else:
     # Mac/Linux GCC compile time arguments
     extra_compile_args = ["-std=c++17", "-pthread", "-g", "-O3", "-fPIC", "-Wno-unused"]
 
+install_requires = [f'numpy>={numpy.__version__},<2']
+if sys.hexversion < 0x03080000:
+    install_requires.append('importlib-resources; python_version<3.8')
+
+depends = [f for f in glob.glob('include/**', recursive=True) if not os.path.isdir(f)]
+
 setuptools.setup(
-    name="dbscan",
-    version=version,
-    author="Yiqiu Wang",
-    author_email="yiqiu_wang@icloud.com",
-    description="Theoretically efficient and practical parallel DBSCAN",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    keywords='cluster clustering density dbscan',
-    url="https://github.com/wangyiqiu/dbscan-python",
-    license='MIT',
     packages=('dbscan',),
-    package_dir={'dbscan': 'src'},
+    package_dir={'dbscan': 'pythonmodule'},
     ext_modules=[Extension(
         "dbscan._dbscan",
         ["src/dbscanmodule.cpp", "src/capi.cpp"],
         language = 'c++',
         extra_compile_args=extra_compile_args,
         include_dirs=[numpy.get_include(), 'include'],
+        depends=depends,
         py_limited_api=True,
-        define_macros=[('Py_LIMITED_API', '0x03020000'), ('NPY_NO_DEPRECATED_API', 'NPY_1_7_API_VERSION')]
+        define_macros=[
+            ('Py_LIMITED_API', '0x03020000'),
+            ('NPY_NO_DEPRECATED_API', 'NPY_1_7_API_VERSION'),
+            ('DBSCAN_VERSION', json.dumps(setuptools_scm.get_version())),
+        ]
     )],
-    classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
-        'Intended Audience :: Science/Research',
-        'Intended Audience :: Developers',
-        "License :: OSI Approved :: MIT License",
-        'Programming Language :: C++',
-        'Programming Language :: Python :: 3',
-        'Topic :: Software Development',
-        'Topic :: Scientific/Engineering',
-    ],
     python_requires=f'>={sys.version_info.major}.{sys.version_info.minor},<4',
-    install_requires=[
-        f'numpy>={numpy.__version__},<2'
-    ],
+    install_requires=install_requires,
     extras_require={
         'scikit-learn': ['scikit-learn'],
         'example': ['scikit-learn', 'matplotlib'],
